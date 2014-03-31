98a6799db590def264507ca4eaea44cd70d70335
Pierre BINAULD <pierre.binauld+github@gmail.com>
2014-03-31 15:48:43 +0200

    Structure initialisation.

---
+++ src/model/lookup/SimulatedAnnealing.java
@@ -0,0 +1,15 @@
+package model.lookup;
+
+import java.util.ArrayList;
+
+import model.data.City;
+
+public class SimulatedAnnealing implements Lookup {
+
+	@Override
+	public void run(ArrayList<City> cities) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ src/model/parser/TSPLIBParser.java
@@ -0,0 +1,12 @@
+package model.parser;
+
+import java.io.File;
+
+import model.data.Country;
+
+public class TSPLIBParser {
+	
+	public Country parse(File file) {
+		return null;
+	}
+}
