49cfe940ff548fcab3af3e556284aea56553243b
Pierre BINAULD <pierre.binauld+github@gmail.com>
2014-05-18 12:42:22 +0200

    GreedyAlgorithm refactoring

--- src/model/lookup/impl/GreedyAlgorithm.java
+++ src/model/lookup/impl/GreedyAlgorithm.java
@@ -20,99 +20,70 @@ public class GreedyAlgorithm extends AbstractBuilderAlgorithm {
 
 	private DistanceService distanceService;
 
+	private int origin;
 	private int sampleSize;
 	private List<Integer> sample = new LinkedList<>();
+	private List<Integer> indexes = new LinkedList<>();
 	private Circuit circuit = new Circuit();
 
-	public GreedyAlgorithm(List<City> cities, DistanceService distanceService, int sampleSize) {
+	public GreedyAlgorithm(DistanceService distanceService, List<City> cities, int sampleSize) {
 		super(cities);
 		this.distanceService = distanceService;
 		this.sampleSize = sampleSize;
 	}
 
-	public static void main(String[] args) throws IOException {
-		TimeService time = new TimeService();
-
-//		File file = new File("/home/pierre/git/Journey/data/wi29.tsp");
-//		File file = new File("/home/pierre/git/Journey/data/ch71009.tsp");
-		File file = new File("/home/pierre/git/Journey/data/ja9847.tsp");
-
-		Country country = TspLibParser.parse(file);
-
-		System.out.println("Name: " + country.getName());
-		System.out.println("Desc: " + country.getDescription());
-		System.out.println("Dim: " + country.getDimension());
-
-//		DistanceService distanceService = new ManhattanDistanceService(country.getCities());
-//		DistanceService distanceService = new LocalStorageDistanceService(new ManhattanDistanceService(country.getCities()));
-//		DistanceService distanceService = new EuclidianDistanceService(country.getCities());
-		DistanceService distanceService = new LocalStorageDistanceService(new EuclidianDistanceService(country.getCities()));
-
-		GreedyAlgorithm g = new GreedyAlgorithm(country.getCities(), distanceService, 10);
-
-		time.start();
-		Circuit c = g.run();
-		System.out.println("Time: " + time.tickInSecond() + "s");
-
-		System.out.println("length: " + c.getLength());
-//		System.out.println(c);
-	}
-
 	@Override
 	public Circuit run() {
-		int count = cities.size();
-		int origin = random(0, count);
-		int root = origin;
-		circuit.setCities(root, cities);
-		List<Integer> indexes = buildCitiesIndex(count);
-		indexes.remove(root);
-		count = indexes.size();
-		System.out.println(indexes.size());
+		
+		int root = init();
 
-		while(indexes.size() > sampleSize) {
-//			System.out.println(indexes);
-			while(sample.size() < sampleSize) {
-				int neighbor = random(0, count);
-				sample.add(neighbor);
-				indexes.remove(neighbor);
-				count = indexes.size();
-			}
-			root = updateCircuitWithNearestNeighbor(root, sample, cities);
+		while (indexes.size() > sampleSize) {
+			updateIndexesSampleList();
+			root = updateCircuitWithNearestNeighbor(root, sample);
 		}
-		while(indexes.size() > 0) {
-//			System.out.println(indexes);
-			root = updateCircuitWithNearestNeighbor(root, indexes, cities);
+		
+		while (indexes.size() > 0) {
+			root = updateCircuitWithNearestNeighbor(root, indexes);
 		}
 
-		circuit.close(Tools.manhattan(cities.get(origin), cities.get(root)));
+		closeCircuit(root);
 
 		return circuit;
 	}
 
-	/**
-	 * Return an integer random value in the range [min, max), where max is not
-	 * included.
-	 *
-	 * @param min
-	 * @param max
-	 * @return
-	 */
-	public int random(int min, int max) {
-		return min + (int) (Math.random() * (max - min));
+	private int init() {
+		int count = cities.size();
+		
+		origin = Tools.random(0, count);
+		indexes = buildCitiesIndex(count);
+		circuit.setCities(origin, cities);
+		indexes.remove(origin);
+		
+		return origin;
 	}
 
-	private int updateCircuitWithNearestNeighbor(int root, List<Integer> neighbors, List<City> cities) {
-//		City rootCity = cities.get(root);
+	private void closeCircuit(int root) {
+		circuit.close(distanceService.getDistance(origin, root));
+	}
+
+	private void updateIndexesSampleList() {
+		while (sample.size() < sampleSize) {
+			int neighbor = Tools.random(0, indexes.size());
+			sample.add(neighbor);
+			indexes.remove(neighbor);
+		}
+	}
+
+	private int updateCircuitWithNearestNeighbor(int root, List<Integer> neighbors) {
 		int nearest = 0;
 		int distance = 0;
 		int test;
 		int count = neighbors.size();
 		int indexNearest = 0;
 
-		for(int n = 0; n < count; n++) {
-//			test = Tools.manhattan(rootCity, cities.get(neighbors.get(n)));
+		for (int n = 0; n < count; n++) {
 			test = distanceService.getDistance(root, neighbors.get(n));
-			if(test < distance || distance == 0) {
+			if (test < distance || distance == 0) {
 				distance = test;
 				nearest = neighbors.get(n);
 				indexNearest = n;
@@ -126,9 +97,40 @@ public class GreedyAlgorithm extends AbstractBuilderAlgorithm {
 
 	private List<Integer> buildCitiesIndex(int size) {
 		List<Integer> cities = new LinkedList<>();
-		for(int i = 0; i < size; i++) {
+		for (int i = 0; i < size; i++) {
 			cities.add(i);
 		}
 		return cities;
+	}
+
+	public static void main(String[] args) throws IOException {
+		TimeService time = new TimeService();
+
+		// File file = new File("/home/pierre/git/Journey/data/wi29.tsp");
+		// File file = new File("/home/pierre/git/Journey/data/ch71009.tsp");
+		File file = new File("/home/pierre/git/Journey/data/ja9847.tsp");
+
+		Country country = TspLibParser.parse(file);
+
+		System.out.println("Name: " + country.getName());
+		System.out.println("Desc: " + country.getDescription());
+		System.out.println("Dim: " + country.getDimension());
+
+		// DistanceService distanceService = new
+		// ManhattanDistanceService(country.getCities());
+		// DistanceService distanceService = new LocalStorageDistanceService(new
+		// ManhattanDistanceService(country.getCities()));
+		// DistanceService distanceService = new
+		// EuclidianDistanceService(country.getCities());
+		DistanceService distanceService = new LocalStorageDistanceService(new EuclidianDistanceService(country.getCities()));
+
+		GreedyAlgorithm g = new GreedyAlgorithm(distanceService, country.getCities(), 10);
+
+		time.start();
+		Circuit c = g.run();
+		System.out.println("Time: " + time.tickInSecond() + "s");
+
+		System.out.println("length: " + c.getLength());
+		// System.out.println(c);
 	}
 }
--- src/model/tools/Tools.java
+++ src/model/tools/Tools.java
@@ -1,13 +1,17 @@
 package model.tools;
 
-import model.data.City;
 
 public class Tools {
 
-	public static int manhattan(City city1, City city2) {
-		return (int) Math.round(
-				Math.abs(city1.getPosition().getX() - city2.getPosition().getX()) +
-						Math.abs(city1.getPosition().getY() - city2.getPosition().getY())
-		);
+	/**
+	 * Return an integer random value in the range [min, max), where max is not
+	 * included.
+	 * 
+	 * @param min
+	 * @param max
+	 * @return
+	 */
+	public static int random(int min, int max) {
+		return min + (int) (Math.random() * (max - min));
 	}
 }
